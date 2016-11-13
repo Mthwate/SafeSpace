4b0d1a29370d1ef3454931d0a5227597ad32df2b
Matthew Benkart <mthwate@mthwate.com>
2016-11-12 22:36:33 -0500

    Added a getUserRating function

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
 var comments = document.body.getElementsByClassName("comment-renderer");
 
+var getUserRating = function(user, funct) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("POST", "https://lrceivshje.execute-api.us-east-1.amazonaws.com/prod/resource", true);
+	xhttp.send('{ "user": "' + user + '" }');
+	xhttp.onload = function () {
+		funct(xhttp.responseText);
+	};
+};
+
 Array.prototype.forEach.call(comments, function(comment) {
 	console.log(comment)
 });
