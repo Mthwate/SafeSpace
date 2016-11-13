f0fa5552ac2e031528bbf8c5269dce2736353625
Matthew Benkart <mthwate@mthwate.com>
2016-11-13 10:52:33 -0500

    Moved common functionality to common.js

--- chrome/background.js
+++ chrome/background.js
@@ -1,48 +1,4 @@
-var lambdaUrl = "https://1pvxwe83ta.execute-api.us-east-1.amazonaws.com/prod/";
-
 var threshold = -1;
-
-var getRating = function(user, funct) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", lambdaUrl + "getrating", true);
-	xhttp.send('{ "user" : "' + user + '" }');
-	xhttp.onload = function () {
-		funct(parseInt(xhttp.responseText));
-	};
-};
-
-var login = function(user, pass, funct) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", lambdaUrl + "login", true);
-	xhttp.send('{ "user" : "' + user + '", "pass" : "' + pass + '" }');
-	xhttp.onload = function () {
-		funct(xhttp.responseText == "true");
-	};
-};
-
-var register = function(user, pass, funct) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", lambdaUrl + "register", true);
-	xhttp.send('{ "user" : "' + user + '", "pass" : "' + pass + '" }');
-	xhttp.onload = function () {
-		funct(xhttp.responseText == "true");
-	};
-};
-
-var report = function(targetUser, rating, comment, funct) {
-    var srcUser = localStorage.user;
-    var pass = localStorage.pass;
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", lambdaUrl + "register", true);
-	xhttp.send('{ "sourceuser" : "' + srcUser + '", "pass" : "' + pass + '", "targetuser" : "' + targetUser + '", "rating" : "' + rating + '", "comment" : "' + comment + '", }');
-	xhttp.onload = function () {
-		funct(xhttp.responseText == "true");
-	};
-};
-
-
-
-
 
 function changeText (text){
 	//text.style.display = "none";	// Hide the user's text
