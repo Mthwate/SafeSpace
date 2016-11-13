3dacf99d386d5c7ee6c486fea63590a42fb023ff
tjking43 <tjking49@gmail.com>
2016-11-12 21:25:36 -0500

    this is the code to give permissions to the server

--- Script1.js
+++ Script1.js
@@ -1,5 +1,5 @@
 // JavaScript source code
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
-xhr.open("GET", chrome.extension.getURL('/this is the adress'), true);
+xhr.open("GET", chrome.extension.getURL('/this is the adress\'), true);
 xhr.send();
