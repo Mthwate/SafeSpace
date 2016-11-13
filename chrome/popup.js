40d63d6d75d71a79c4ced41b5e5e9389b52f6ad9
Tyler Small <tjking49@gmail.com>
2016-11-13 08:48:39 -0500

    Update to Jquery

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,91 +1,168 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var lambdaUrl = "https://lrceivshje.execute-api.us-east-1.amazonaws.com/prod/"
+console.log("going");
+$(document).ready(function () {
+    var comments = $(".comment-renderer");
+    console.log(comments);
+    var getRating = function (user, funct) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", lambdaUrl + "getrating", true);
+        xhttp.send('{ "user" : "' + user + '" }');
+        xhttp.onload = function () {
+            funct(xhttp.responseText);
+        };
+    };
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+    var login = function (user, pass, funct) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", lambdaUrl + "login", true);
+        xhttp.send('{ "user" : "' + user + '", "pass" : "' + pass + '" }');
+        xhttp.onload = function () {
+            funct(xhttp.responseText);
+        };
+    };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+    var register = function (user, pass, funct) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", lambdaUrl + "register", true);
+        xhttp.send('{ "user" : "' + user + '", "pass" : "' + pass + '" }');
+        xhttp.onload = function () {
+            funct(xhttp.responseText);
+        };
+    };
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+    var report = function (srcUser, pass, targetUser, rating, comment, funct) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", lambdaUrl + "register", true);
+        xhttp.send('{ "sourceuser" : "' + srcUser + '", "pass" : "' + pass + '", "targetuser" : "' + targetUser + '", "rating" : "' + rating + '", "comment" : "' + comment + '", }');
+        xhttp.onload = function () {
+            funct(xhttp.responseText);
+        };
+    };
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-    Object.onclick = function () {//my script is where all variables are.
-
-    }
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
+    var threshold = 3;
+    Array.prototype.forEach.call(comments, function (comment) {
+        //console.log(comment);
+        // Grab element, done with comment
+        // Grab username
+        var username = comment.getElementsByClassName("comment-renderer-header")[0].getElementsByTagName("a")[0].textContent;
+        // Check user name
+        console.log(username);
+        var rate = rating();
+        // If(rating >= threshold}
+        if (rate >= threshold) {
+            // Black Out comment for now
+            var text = comment.getElementsByClassName("comment-renderer-text-content")[0];
+            changeText(text);
+            //comment.style.color = "black";
+            //console.log("Set background to black");
+        }
     });
-  });
-});
+
+    function rating() {
+        //console.log("Inside the rating function");
+        return 5;
+    }
+
+    function changeText(text) {
+        //text.style.display = "none";	// Hide the user's text
+        text.style.visibility = "hidden";
+        // Add text inline
+        //var warningText = document.createTextNode("User has bad rating...");
+        var warningText = document.createElement("div");
+        warningText.style.color = "red";
+        warningText.textContent = "User has a bad rating!";
+        var btn = document.createElement("input");
+        btn.type = "button";
+        btn.onclick = function () {
+            if (this.value == "View Anyways") {
+                this.parentNode.childNodes[0].style.visibility = "visible";
+                this.value = "Hide Comment";
+            }
+            else {
+                this.parentNode.childNodes[0].style.visibility = "hidden";
+                this.value = "View Anyways";
+            }
+        };
+        btn.value = "View Anyways";
+        // Add a button
+        //text.insertAdjacentHTML('afterend', "<input type=\"button\" onclick=\"buttonHandler(event.target)\" value=\"View Anyways\">");
+        //text.insertAdjacentHTML('afterend', btn.htmlText);
+
+        //text.insertAdjacentHTML('afterend', "User has a bad rating...");
+        text.parentNode.appendChild(warningText);
+        text.parentNode.appendChild(btn);
+        // Add a listener to the button
+    }
+
+
+    // Add a button handler
+    function buttonHandler(text) {
+        if (this.value == "View Anyways") {
+            text.style.visibility = "visible";
+            this.value = "Hide Comment";
+        }
+        else {
+            text.style.visibility = "hidden";
+            this.value = "View Anyways";
+        }
+        //text.getElementsByTagName("INPUT")[0].textContent = "Hide comment";
+    }
+    function getCurrentTabUrl(callback) {
+        // Query filter to be passed to chrome.tabs.query - see
+        var queryInfo = {
+            active: true,
+            currentWindow: true
+        }
+    };
+
+    chrome.tabs.query(queryInfo, function(tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+
+        console.assert(typeof url == 'string', 'tab.url should be a string');
+
+        callback(url);
+    });
+    x.onerror = function() {
+        errorCallback('Network error.');
+    };
+    x.send();
+
+    function renderStatus(statusText) {
+        document.getElementById('status').textContent = statusText;
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        getCurrentTabUrl(function(url) {
+            // Put the image URL in Google search.
+            renderStatus('Performing Google Image search for ' + url);
+
+            getImageUrl(url, function(imageUrl, width, height) {
+
+                renderStatus('Search term: ' + url + '\n' +
+                    'Google image search result: ' + imageUrl);
+                var imageResult = document.getElementById('image-result');
+                imageResult.width = width;
+                imageResult.height = height;
+                imageResult.src = imageUrl;
+                imageResult.hidden = false;
+
+            }, function(errorMessage) {
+                renderStatus('Cannot display image. ' + errorMessage);
+            });
+        });
+    });
+    console.log("going");
+    var e = document.getElementById("login");
+    console.log("going1");
+    if (e.onclick == "logingo") {
+        console.log("going2");
+        e.onclick = login(e.getElementsByTagName("uname"), e.getElementsByTagName("psw"));
+    }
+    var f = document.getElementById("register");
+    console.log("going3");
+    if (f.onclick == "regigo") {
+        console.log("going4");
+     
+        f.onclick = register(f.getElementsByTagName("uname"), f.getElementsByTagName("psw"));
+    }})
