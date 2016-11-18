acabf1e5c87217413beec6e6db6c31249a701909
Matthew Benkart <mthwate@mthwate.com>
2016-11-17 21:35:06 -0500

    Removed unused script

--- chrome/Script1.js
+++
@@ -1,5 +0,0 @@
-// JavaScript source code
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
-xhr.open("GET", chrome.extension.getURL('/this is the adress\'), true);
-xhr.send();
